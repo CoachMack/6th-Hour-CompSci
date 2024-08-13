a097c7271fc62c9ed52b85b5718d40af924013da
Jeremy McKellips <jmckellips@wpsok.org>
2024-08-13 13:46:23 -0500

    Initial commit

---
+++ PB and J.py
@@ -0,0 +1,28 @@
+#Name: 6th Hour Class
+#Assignment: PB&J
+
+
+#ingredients
+bread = 2
+peanutButter = 1
+jelly = float(0.5)
+ingredientTotal = bread + peanutButter + jelly
+
+#Utensils and Plate
+plate = 1
+knife = 2
+jar = 2
+
+#Prep work
+print("Step 1: Put the bread on the plate.")
+print("Step 2: Open the jar of peanut butter.")
+print("Step 3: Open the jar of jelly.")
+
+#Make the sandwich
+print("Step 4: Scoop the peanut butter with a knife and spread it on the bread.")
+print("Step 5: Scoop the jelly with a knife and spread it on another piece of bread.")
+print("Put the pieces of bread together.")
+
+#Plate the food
+print("Put the complete sandwich on the plate.")
+print("The total ingredients are", ingredientTotal)
