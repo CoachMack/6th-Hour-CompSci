e95fed5c3930ef2c6f8fb586451c4a69389fd78d
Coach Mack <jmckellips@wpsok.org>
2024-08-22 13:49:56 -0500

    Added HW2 homework notes

---
+++ HW2.py
@@ -0,0 +1,13 @@
+#Name: Coach Mack
+#Class: 6th Hour
+#Assignment: HW2
+
+
+#Print Hello World!
+print("Hello World!")
+#Make an input that asks for your name
+name = input("What is your name?")
+print("Hello",name)
+#Make two inputs that asks for two integers
+
+#Add those two integers together and print them
