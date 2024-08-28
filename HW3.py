b30b022860b2e4d87a6ba09f9cba4c8f65ab18b1
Coach Mack <jmckellips@wpsok.org>
2024-08-28 12:02:47 -0500

    Added HW3 Assignment

---
+++ HW3.py
@@ -0,0 +1,20 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW3
+#Code Used:
+
+
+#Objectives:
+
+#Print Hello World!
+
+#Create a list with 5 strings containing 5 different names in it
+#Append a new name onto the Name List
+#Print out the 4th name on the list
+
+#Create a list with 4 different integers in it
+#Insert a new integer into the 2nd spot
+#Print the Integer List
+#Sort the list from lowest to highest
+#Add the 1st three numbers on the sorted list together
+#Print the sum
