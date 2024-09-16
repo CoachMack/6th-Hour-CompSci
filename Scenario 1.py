114f759bd6a73500acf3d6810937eca4dde47d4b
Coach Mack <jmckellips@wpsok.org>
2024-09-16 13:21:09 -0500

    Added Scenario 1 Assignment

--- Scenario 1.py
+++ Scenario 1.py
@@ -7,7 +7,7 @@
 #has asked you to create a nested dictionary containing five enemy
 #creatures (and their properties) for combat testing. Additionally,
 #the testers are asking for a way to input changes to the enemy's
-#damage for balancing, as well as having it print those changes to
+#damage values for balancing, as well as having it print those changes to
 #confirm they went through.
 
 #It is up to you to decide what properties
