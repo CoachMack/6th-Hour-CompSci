f0b5e0b110d69887d1415f2c6f2eea7ea9b1d08e
Coach Mack <jmckellips@wpsok.org>
2024-09-16 12:25:01 -0500

    Added Scenario 1 Assignment

---
+++ Scenario 1.py
@@ -0,0 +1,14 @@
+#Name:
+#Class: 5th Hour
+#Assignment: Scenario 1
+
+#Scenario 1:
+#You are a programmer for a fledgling game developer. Your team lead
+#has asked you to create a nested dictionary containing five enemy
+#creatures (and their properties) for combat testing. Additionally,
+#the testers are asking for a way to input changes to the enemy's
+#damage for balancing, as well as having it print those changes to
+#confirm they went through.
+
+#It is up to you to decide what properties
+#are important and the theme of the game.
