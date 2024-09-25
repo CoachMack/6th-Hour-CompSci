23fd63a2d04738dc78a621c6e10cb3c25546b802
Coach Mack <jmckellips@wpsok.org>
2024-09-25 14:00:20 -0500

    Added Scenario 1 and 2 lecture notes

--- Scenario 1.py
+++ Scenario 1.py
@@ -20,3 +20,46 @@
 #Your team lead has asked you to fix the bug and make sure your enemy damage
 #can be subtracted properly while testing.
 #(HINT: The player's health is stored as an integer.)
+
+#Make an enemy dictionary containing 5 enemies
+enemyDict = {
+    "goblin" : {
+        "Health" : 6,
+        "AC" : 10,
+        "Damage" : 4
+    },
+    "mimic": {
+        "Health" : 17,
+        "AC" : 13,
+        "Damage" : 10
+    },
+    "orc": {
+        "Health" : 22,
+        "AC" : 14,
+        "Damage" : 17
+    },
+    "dragon": {
+        "Health" : 78,
+        "AC" : 16,
+        "Damage" : 25
+    },
+    "mindflayer": {
+        "Health" : 101,
+        "AC" : 17,
+        "Damage" : 33
+    },
+}
+
+#Allow user input to change damage value of enemies
+enemyDict["goblin"]["Damage"] = int(input("Change the damage for Goblin: "))
+enemyDict["mimic"]["Damage"] = int(input("Change the damage for Mimic: "))
+enemyDict["orc"]["Damage"] = int(input("Change the damage for Orc: "))
+enemyDict["dragon"]["Damage"] = int(input("Change the damage for Dragon: "))
+enemyDict["mindflayer"]["Damage"] = int(input("Change the damage for Mindflayer: "))
+
+#Print the changes to confirm they went through
+print("The new damage for Goblin is:", enemyDict["goblin"]["Damage"])
+print("The new damage for Mimic is:", enemyDict["mimic"]["Damage"])
+print("The new damage for Orc is:", enemyDict["orc"]["Damage"])
+print("The new damage for Dragon is:", enemyDict["dragon"]["Damage"])
+print("The new damage for Mindflayer is:", enemyDict["mindflayer"]["Damage"])
--- Scenario 2.py
+++ Scenario 2.py
@@ -5,7 +5,7 @@
 #Scenario 2:
 #The programmer in charge of the player character party stats is
 #having some issues with their code. Despite rigorous testing,
-#they are inexperienced and can't seem to figure out why it damage
+#they are inexperienced and can't seem to figure out why the damage
 #testing isn't working. Your team lead has asked you to help by fixing
 #the party dictionary, insert an enemy into the code below, and
 #testing to see if a player character can damage the with a printed
@@ -17,37 +17,47 @@ partyDictionary = {
         "Race" : "Githyanki",
         "Class" : "Fighter",
         "Background" : "Soldier",
-        "Health" : "12",
-        "AC" : "17",
-        "Damage" : "10",
+        "Health" : 12,
+        "AC" : 17,
+        "Damage" : 10,
     },
     "Shadowheart" : {
         "Race" : "Half-Elf",
         "Class" : "Cleric",
         "Background" : "Acolyte",
-        "Health" : "10",
-        "AC" : "14",
-        "Damage" : "5",
+        "Health" : 10,
+        "AC" : 14,
+        "Damage" : 5,
     },
     "Gale" : {
         "Race" : "Human",
         "Class" : "Wizard",
         "Background" : "Sage",
-        "Health" : "8",
-        "AC" : "14",
-        "Damage" : "17",
+        "Health" : 8,
+        "AC" : 14,
+        "Damage" : 17,
     },
     "Astarion" : {
         "Race" : "High Elf",
         "Class" : "Rogue",
         "Background" : "Charlatan",
-        "Health" : "10",
-        "AC" : "14",
-        "Damage" : "12",
+        "Health" : 10,
+        "AC" : 14,
+        "Damage" : 12,
     }
 }
 
 #Enemy Dictionary goes here
-
+enemyDict = {
+    "goblin" : {
+        "Health" : 6,
+        "AC" : 10,
+        "Damage" : 4
+    }
+}
 
 #Test the damage here by subtracting a party member's damage from the enemy's health.
+print(enemyDict["goblin"]["Health"] - partyDictionary["Astarion"]["Damage"])
+print(enemyDict["goblin"]["Health"] - partyDictionary["LaeZel"]["Damage"])
+print(enemyDict["goblin"]["Health"] - partyDictionary["Gale"]["Damage"])
+print(enemyDict["goblin"]["Health"] - partyDictionary["Shadowheart"]["Damage"])
