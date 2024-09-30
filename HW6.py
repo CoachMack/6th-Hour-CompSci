c829fdcdc9aa151f3f93d8fc537a841d3210a314
Coach Mack <jmckellips@wpsok.org>
2024-09-30 12:08:08 -0500

    Added HW6

---
+++ HW6.py
@@ -0,0 +1,26 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW6
+
+
+#Objectives
+
+#1. Print Hello World!
+
+#2. Create a variable named num and assign it a variable.
+
+#3. Create a nested if statement follows this structure:
+
+#If num is divisible by 2
+#   if num is divisible by 3
+#       print the result of num being divided by 2
+#       print the result of num being divided by 3
+#   else
+#       print that it is not divisible by 3
+#       print the result of num being divided by 2
+#else
+#   if num is divisible by 3
+#       print that num is not divisible by 2
+#       print the result of num being divided by 3
+#   else
+#       print that neither is divisible by 2 or 3
