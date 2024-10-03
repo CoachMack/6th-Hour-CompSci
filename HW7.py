6ea1b97bcb6244279faa6de1f5665bfbeff93cd3
Coach Mack <jmckellips@wpsok.org>
2024-10-03 12:03:48 -0500

    Added HW7 assignment

---
+++ HW7.py
@@ -0,0 +1,15 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW7
+
+#1. Print Hello World!
+
+#2. Create three different boolean variables named wifi, login, and admin.
+
+#3. Create a separate integer variable that denotes the number of times
+#someone with admin credentials has logged in.
+
+#4. Create a nested if statement that checks to see if wifi is true,
+#login is true, and admin is true. If they are all true, print a
+#welcome message and increase the integer variable by one. If one of them
+#is false, print an error message telling them which one they are "missing".
