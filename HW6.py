e17710a20eb1c4fd3178e6adee1ea885f0c29208
Coach Mack <jmckellips@wpsok.org>
2024-10-08 14:06:25 -0500

    Added HW6 and HW7 notes

--- HW6.py
+++ HW6.py
@@ -6,21 +6,22 @@
 #Objectives
 
 #1. Print Hello World!
-
+print("Hello World!")
 #2. Create a variable named num and assign it a number.
-
+num = 36
 #3. Create a nested if statement follows this structure:
 
-#If num is divisible by 2
-#   if num is divisible by 3
-#       print the result of num being divided by 2
-#       print the result of num being divided by 3
-#   else
-#       print that it is not divisible by 3
-#       print the result of num being divided by 2
-#else
-#   if num is divisible by 3
-#       print that num is not divisible by 2
-#       print the result of num being divided by 3
-#   else
-#       print that neither is divisible by 2 or 3
+
+if num % 2 == 0: #If num is divisible by 2
+    if num % 3 == 0: #if num is divisible by 3
+        print(num / 2) #print the result of num being divided by 2
+        print(num / 3) #print the result of num being divided by 3
+    else: #else
+        print("Number not divisible by 3") #print that it is not divisible by 3
+        print(num / 2) #print the result of num being divided by 2
+else: #else
+    if num % 3 == 0: #if num is divisible by 3
+        print("Number not divisible by 2") #print that num is not divisible by 2
+        print(num / 3) #print the result of num being divided by 3
+    else: #else
+        print("Number not divisible by 2 or 3.") #print that neither is divisible by 2 or 3
--- HW7.py
+++ HW7.py
@@ -3,13 +3,28 @@
 #Assignment: HW7
 
 #1. Print Hello World!
-
+print("Hello World!")
 #2. Create three different boolean variables named wifi, login, and admin.
-
+watermelon = True
+lewis = True
+GTAVI = True
 #3. Create a separate integer variable that denotes the number of times
 #someone with admin credentials has logged in.
-
+jeffery = 7
 #4. Create a nested if statement that checks to see if wifi is true,
 #login is true, and admin is true. If they are all true, print a
 #welcome message and increase the integer variable by one. If one of them
 #is false, print an error message telling them which one they are "missing".
+
+if watermelon == True: #Create an if statement that checks to see if watermelon is true
+    if lewis == True: #Create an if statment that checks to see if lewis is true
+        if GTAVI == True: #Create an if statement that checks to see if GTAVI is released
+            print("Welcome Rockstar!") #Create Welcome message
+            jeffery += 1 #Increase jeffery by one
+            print(jeffery) #Print jeffery
+        else: #Error message for GTA6
+            print("GTA VI is not released yet!")
+    else: #Error message for Lewis
+        print("Lewis got benched!")
+else: #Error message for Watermelon
+    print("No watermelon in the bowl.")
