03cd1f1971c46d71eec966ac8fea84bed507c960
Coach Mack <jmckellips@wpsok.org>
2024-10-22 09:00:54 -0500

    Added HW8 and HW9

---
+++ HW8.py
@@ -0,0 +1,44 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW8
+
+#1. Print Hello World!
+
+#2. Take the variables below and change the name of the variables to match the type of variable they are: string, integer, boolean, or list.
+
+#For example, the first one, Var1 = 4, should be:
+#integerVar1 = 4 or intVar1 = 4
+
+#DO NOT CHANGE THE VALUE OF THE VARIABLE.
+
+Var1 = 4
+Var3 = "Box"
+Var4 = True
+Var5 = "Nineteen"
+Var6 = "Rope"
+Var7 = 19
+Var8 = "19"
+Var9 = ["Window", "Apple", "Penguin"]
+Var10 = 2
+Var11 = True
+Var12 = False
+Var13 = "True"
+Var14 = "False"
+Var15 = [1, 0]
+Var16 = [1]
+Var17 = [0]
+Var18 = 5
+Var19 = "The"
+Var20 = "Game"
+Var21 = 3
+Var22 = 2
+Var23 = 1
+Var24 = ["Liftoff", True]
+Var25 = Var11
+Var26 = Var12
+Var27 = Var13
+Var28 = Var14
+Var29 = Var15
+Var30 = 6
+
+#3. Take all the variables you labeled as "integer", add them together, and print the result.
---
+++ HW9.py
@@ -0,0 +1,9 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
