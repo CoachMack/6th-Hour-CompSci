28a33dc4ad258f383f7a1f6ae503b66f12f1bf84
Coach Mack <jmckellips@wpsok.org>
2024-10-23 13:45:50 -0500

    Added HW8 work

--- HW8.py
+++ HW8.py
@@ -3,42 +3,43 @@
 #Assignment: HW8
 
 #1. Print Hello World!
-
-#2. Take the variables below and change the name of the variables to match the type of variable they are: string, integer, boolean, or list.
+print("Hello World!")
+#2. Take the variables below and change the name of the variables to match
+#the type of variable they are: string, integer, boolean, or list.
 
 #For example, the first one, Var1 = 4, should be:
 #integerVar1 = 4 or intVar1 = 4
 
 #DO NOT CHANGE THE VALUE OF THE VARIABLE.
 
-Var1 = 4
-Var3 = "Box"
-Var4 = True
-Var5 = "Nineteen"
-Var6 = "Rope"
-Var7 = 19
-Var8 = "19"
-Var9 = ["Window", "Apple", "Penguin"]
-Var10 = 2
-Var11 = True
-Var12 = False
-Var13 = "True"
-Var14 = "False"
-Var15 = [1, 0]
-Var16 = [1]
-Var17 = [0]
-Var18 = 5
-Var19 = "The"
-Var20 = "Game"
-Var21 = 3
-Var22 = 2
-Var23 = 1
-Var24 = ["Liftoff", True]
-Var25 = Var11
-Var26 = Var12
-Var27 = Var13
-Var28 = Var14
-Var29 = Var15
-Var30 = 6
+intVar1 = 4
+strVar3 = "Box"
+boolVar4 = True
+strVar5 = "Nineteen"
+strVar6 = "Rope"
+intVar7 = 19
+strVar8 = "19"
+listVar9 = ["Window", "Apple", "Penguin"]
+intVar10 = 2
+boolVar11 = True
+boolVar12 = False
+strVar13 = "True"
+strVar14 = "False"
+listVar15 = [1, 0]
+listVar16 = [1]
+listVar17 = [0]
+intVar18 = 5
+strVar19 = "The"
+strVar20 = "Game"
+intVar21 = 3
+intVar22 = 2
+intVar23 = 1
+listVar24 = ["Liftoff", True]
+boolVar25 = boolVar11
+boolVar26 = boolVar12
+strVar27 = strVar13
+strVar28 = strVar14
+listVar29 = listVar15
+intVar30 = 6
 
 #3. Take all the variables you labeled as "integer", add them together, and print the result.
