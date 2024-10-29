5c7197fbcccd15ce1515f8337b862dea0c76bac2
Coach Mack <jmckellips@wpsok.org>
2024-10-29 12:19:39 -0500

    Added HW10 and HW11

---
+++ HW10.py
@@ -0,0 +1,12 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW10
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
