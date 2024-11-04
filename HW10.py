7c5fe9eeafb8d9a347e309c1ef39d333bf0a948c
Coach Mack <jmckellips@wpsok.org>
2024-11-04 13:55:58 -0600

    Added HW10 and HW11 answer notes

--- HW10.py
+++ HW10.py
@@ -1,13 +1,28 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW10
 
 #1. Create a while loop with variable i that counts down from 5 to 0
 #and then prints "Hello World!" at the end.
+i = 5
+while i >= 0:
+    print(i)
+    i -= 1
+else:
+    print("Hello World!")
 
 #2. Create a while loop that prints only even numbers
 #between 1 and 30.
 #(HINT: Look back to HW6 on how to see if a number is divisible by another)
+j = 1
+while j <= 30:
+    if j % 2 == 0:
+        print(j)
+    j += 1
 
 #3. Create a while loop that asks for a number input until the user
 #inputs the number 0.
+
+k = int(input("Enter Zero to Continue!"))
+while k != 0:
+    k = int(input("Wrong number! Enter Zero to Continue!"))
--- HW11.py
+++ HW11.py
@@ -1,4 +1,4 @@
-#Name:
+#Name: Coach Mack
 #Class: 5th Hour
 #Assignment: HW11
 
@@ -10,3 +10,49 @@
 
 #Create a while loop that follows the rules of the game.
 #(HINT: Look back to HW6 on how to see if a number is divisible by another)
+
+#THIS IS THE BAD METHOD
+
+i = 1
+while i <= 100:
+    if i % 3 == 0 and not i % 5 == 0:
+        print("Fizz")
+    if i % 5 == 0 and not i % 3 == 0:
+        print("Buzz")
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    if not i % 3 == 0 and not i % 5 == 0:
+        print(i)
+    i += 1
+
+#This is the good method
+
+i = 1
+
+while i <= 100:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i += 1
+
+
+#This is the "true ending" method
+i = 1
+
+while i <= 1000:
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if i % 7 == 0:
+        output += "Fazz"
+    if output == "":
+        output = i
+    print(output)
+    i += 1
