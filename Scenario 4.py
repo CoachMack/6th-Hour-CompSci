367132488de40b9daeb12b7821cf42580b74d683
Coach Mack <jmckellips@wpsok.org>
2024-11-18 12:31:48 -0600

    Added Scenario 4

---
+++ Scenario 4.py
@@ -0,0 +1,12 @@
+#Name:
+#Class: 6th Hour
+#Assignment: Scenario 4
+
+#Scenario 4:
+#Due to scope creep leading to high development costs, the RPG you were working on has been
+#shelved for the time being. You have been transferred to a new team working on a mobile game
+#that allows you to dress up a model and rate other models.
+
+#They want to start prototyping the rating system and are asking you to make it.
+#This prototype needs to allow the user to input the number of players, let each player rate
+#a single model from 1 to 5, and then give the average score of all of the ratings.
