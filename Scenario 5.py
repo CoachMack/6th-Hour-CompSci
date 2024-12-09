0a0a52fa5ced01e88ee02cee554f7229a64dd7e9
Coach Mack <jmckellips@wpsok.org>
2024-12-09 11:58:14 -0600

    Added Scenario 5

---
+++ Scenario 5.py
@@ -0,0 +1,26 @@
+#Name:
+#Class: 6th Hour
+#Assignment: Scenario 5
+
+#Scenario 5
+#You're all part of a new development team and the big wigs want to see what you are all capable of.
+#They want you to develop whatever you want to develop, but there's only one problem:
+#the same big wigs only bought enough computers for half of you. Because of this, you will
+#all be split into teams of two. One serving as the brain (not using the computer),
+#one serving as the hands (using the computer).
+
+#The Teams are as such:
+
+#Team 1: Logan B (brain), CJ (hand)
+#Team 2: Jax (brain), Colin (hand)
+#Team 3: Kyle (brain), Evan (hand)
+#Team 4: Ryan (brain), Seeley (hand)
+#Team 5: Logan Z (brain), Elijah (hand)
+
+
+#You have until the Scenario is due to brainstorm an idea together, create a flowchart, and then
+#turn that flowchart into functioning code. The code itself must have at least: 1 dictionary or list,
+#1 loop, 2 if statements (elif and else statements tied to it does not count towards the total),
+#1 variable with a user input, and 1 form of an assignment operator. You are free to add whatever else
+#you feel is necessary to complete your concept. You will be graded based on how those ideas are
+#implemented together.
