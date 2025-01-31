133a7727a27e63f039aaf4d7c7e3a22ef8e84a05
Jeremy McKellips <jmckellips@wpsok.org>
2025-01-31 08:59:05 -0600

    Added HW19

---
+++ HW19.py
@@ -0,0 +1,9 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+
+#2. Call the functions here and run HW19.
+
+#3. Delete all calls from HW15 and run HW19 again.
