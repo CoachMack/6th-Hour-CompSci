046f2a552c3925822dbab5abd4ec0cb1e8852a93
Jeremy McKellips <jmckellips@wpsok.org>
2025-02-02 19:02:00 -0600

    Added Scenario 6 and Scenario 7

---
+++ Scenario_6.py
@@ -0,0 +1,13 @@
+#Name:
+#Class: 6th Hour
+#Assigment: Scenario 6
+import random
+
+#After an extended leave, the team lead for the RPG developer is back, and he wants to continue the project.
+#He wants to prototype the character creation model but first needs something that rolls stats for the characters.
+#He wants you to make a function that rolls 4 six-sided dice (d6), sorts them from highest to lowest, and then adds the
+#highest 3 together. He then wants you to add that result to a list outside the function. He wants you to run that function
+#5 more times (six times total) and print all six stats.
+
+#Once that is done, to ensure that the average of the statblock is fair (somewhere roughly between 12-13), he wants you
+#to plug it into a calculator (Scenario 7) and print the average.
---
+++ Scenario_7.py
@@ -0,0 +1,16 @@
+#Name:
+#Class: 6th Hour
+#Assignment: Scenario 7
+
+#Import all of Scenario 6 here
+
+listAverage = 0
+
+def final_average():
+    global listAverage
+    listAverage = #Calculate the sum of the list by the length of the list here
+    return listAverage
+
+final_average()
+
+print(listAverage)
