3b5ecbaeddd35761d193606add62425784dc2531
Jeremy McKellips <jmckellips@wpsok.org>
2025-03-06 10:58:34 -0600

    Added HW22

---
+++ HW22.py
@@ -0,0 +1,15 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW22
+
+#1. Create a class containing a def function that inits self and 3 other attributes for store items (stock, cost, and weight).
+
+#2. Make 3 objects to serve as your store items and give them values to those 3 attributes defined in the class.
+
+#3. Print the stock of all three objects and the cost of the second store item.
+
+#4. Make a def function within the class that doubles the cost of the second store item and print the new cost below the original cost print statement.
+
+#5. Directly change the stock of the third store item to approx. 1/4th the original stock and then print the new stock amount.
+
+#6. Delete the first store item and then attempt to print the weight of the first store item. Create a try/except catch to fix the error.
