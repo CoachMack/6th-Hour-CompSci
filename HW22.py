02a303280acd36a2349ebd3dedc12c4af9d80f45
Coach Mack <jmckellips@wpsok.org>
2025-03-24 14:00:38 -0500

    Added HW22 and HW23 Review Notes

--- HW22.py
+++ HW22.py
@@ -1,15 +1,36 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW22
 
 #1. Create a class containing a def function that inits self and 3 other attributes for store items (stock, cost, and weight).
+class StoreItems:
+    def __init__(self, stock, cost, weight):
+        self.stock = stock
+        self.cost = cost
+        self.weight = weight
+
+    def double_cost(self):
+        self.cost *= 2
 
 #2. Make 3 objects to serve as your store items and give them values to those 3 attributes defined in the class.
-
+banana = StoreItems(10, 2, 0.25)
+twinkie = StoreItems(12, 3, 0.1)
+gallonWater = StoreItems(15, 3.25, 2)
 #3. Print the stock of all three objects and the cost of the second store item.
+print("Stock of Bananas:", banana.stock)
+print("Stock of Twinkies:", twinkie.stock)
+print("Stock of Gallon of Water:", gallonWater.stock)
 
+print(f"Cost of Twinkies: {twinkie.cost}")
 #4. Make a def function within the class that doubles the cost an item, double the cost of the second store item, and print the new cost below the original cost print statement.
-
+twinkie.double_cost()
+print(f"New Cost of Twinkies: {twinkie.cost}")
 #5. Directly change the stock of the third store item to approx. 1/4th the original stock and then print the new stock amount.
-
+gallonWater.stock = 4
+print("New Stock of Gallon of Water:", gallonWater.stock)
 #6. Delete the first store item and then attempt to print the weight of the first store item. Create a try/except catch to fix the error.
+del banana
+try:
+    print(banana.weight)
+except NameError:
+    print("Item Not In Stock")
--- HW23.py
+++ HW23.py
@@ -1,23 +1,41 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW23
 
 #1. Import the random and time libraries
+import random, time
 
 #2. Create a class containing a def function that inits self and the 3 attributes health, damage, and speed.
+class Character:
+    def __init__(self, health, damage, speed):
+        self.health = health
+        self.damage = damage
+        self.speed = speed
+
+    def poison(self):
+        for i in range(10):
+            self.health -= random.randint(1,3)
+            print(warrior.health)
+            time.sleep(0.4)
+
+    def healing(self):
+        warrior.health += 30
+        if warrior.health > 100:
+            warrior.health = 100
 
 #3. Make a "warrior" character object with 100 health, 20 damage, and 30 speed. Print the character's initial health below.
-
+warrior = Character(100, 20, 30)
+print(warrior.health)
 #4. Make a def function within the class that loops 10 times. Within this function,
 #make the following loop 10 times: the character takes a random amount of damage from 1 to 6,
 #the new health is printed, a time.sleep delay of one second is done. Call the function to the warrior.
-
+warrior.poison()
 #5. Make a "healer" character object with 60 health, 10 damage, and 30 speed.
-
+healer = Character(60, 10, 30)
 #6. Make a def function within the class that heals the warrior for 30 health. Create an if statement
 #that sets the warrior's health to its max (100) if the healing would bring the warrior's health above that.
 #Call the function to the healer.
-
+healer.healing()
 #7. Print the warrior's final health at the very bottom.
-
+print(f"Warrior's Final Health: {warrior.health}")
 
