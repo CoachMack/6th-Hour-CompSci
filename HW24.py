94e355993abaeff5c73f69eed3d051b5f317cceb
Jeremy McKellips <jmckellips@wpsok.org>
2025-03-25 09:57:00 -0500

    Added HW24

---
+++ HW24.py
@@ -0,0 +1,15 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW24
+
+import random, time
+
+#1. Copy over your class from HW23 and all the functions inside of it, and alter any functions to use self if applicable.
+
+#2. Create a fourth attribute in the class called max_health that has the same values as health
+
+#3. Copy the warrior and healer objects from HW23, and then make two more character objects with the following attribute values: thief (health/max: 50, damage: 30, speed: 40) and mage (health/max: 45, damage:35, speed: 25)
+
+#4. Randomly choose one of the four character objects to take the damage over time function and call the function to them
+
+#5. Determine who lost health by comparing the current health to the max_health and heal that character object by calling your healing function to that object and then print their health afterwards.
